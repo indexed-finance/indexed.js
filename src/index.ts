974f518b5db637586a93e9bfde2e50fa0280f2ac
dillon <dillonkellar@protonmail.com>
2020-11-05 05:30:25 -0600

    Added additional exports, improved getAllHelpers fn

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import { getPools } from './subgraph';
 export * from './multicall';
 import * as bmath from './bmath';
-import { Pool } from './types';
+import { UninitializedPool } from './types';
 import { PoolHelper } from './pool-helper';
+import { InitializerHelper } from './initializer-helper';
 export * from './utils/bignumber';
 export { bmath, getPools, PoolHelper };
 
-export async function getAllHelpers(provider: any): Promise<{
+export async function getAllHelpers(provider: any, userAddress?: string): Promise<{
   initialized: PoolHelper[],
-  uninitialized: Pool[]
+  uninitialized: InitializerHelper[],
 }> {
   const poolDatas = await getPools();
   const initialized: PoolHelper[] = [];
-  const uninitialized: Pool[] = [];
+  const uninitialized: InitializerHelper[] = [];
   for (let poolData of poolDatas) {
     if (poolData.isPublic) {
-      initialized.push(new PoolHelper(provider, poolData));
+      initialized.push(new PoolHelper(provider, poolData, userAddress));
     } else {
-      uninitialized.push(poolData);
+      uninitialized.push(new InitializerHelper(provider, poolData as UninitializedPool, userAddress));
     }
   }
   return { initialized, uninitialized };
