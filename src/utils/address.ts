1049abb2713dc099377046e48fcadbe8ee5b00dc
dillon <dillonkellar@protonmail.com>
2020-11-09 09:55:50 -0600

    Added uniswap pair calculation

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,4 +1,6 @@
-import { getAddress } from "@ethersproject/address";
+import { getAddress, getCreate2Address } from "@ethersproject/address";
+import { keccak256 } from "ethers/lib/utils";
+import { UNISWAP_FACTORY } from "../constants";
 
 export type AddressLike = string | { address: string } | { options: { address: string } };
 
@@ -10,4 +12,19 @@ export function toAddress(obj: AddressLike): string {
 
 export function toChecksum(obj: AddressLike): string {
   return getAddress(toAddress(obj));
+}
+
+const addressToBuffer = (address: string): Buffer => Buffer.from(address.slice(2).padStart(40, '0'), 'hex');
+
+export function sortTokens(tokenA: string, tokenB: string): string[] {
+  return (tokenA < tokenB) ? [tokenA, tokenB] : [tokenB, tokenA];
+}
+
+export function computeUniswapPairAddress(tokenA: string, tokenB: string): string {
+  const initCodeHash = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
+  const [token0, token1] = sortTokens(tokenA, tokenB);
+  const salt = keccak256(
+    Buffer.concat([addressToBuffer(token0), addressToBuffer(token1)])
+  );
+  return getCreate2Address(UNISWAP_FACTORY, salt, initCodeHash);
 }
