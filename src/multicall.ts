3c83c26fbd7627e0a617f343dbe89bab94538a5e
dillon <dillonkellar@protonmail.com>
2020-11-13 12:21:43 -0600

    Added uniswap multicall fns

--- src/multicall.ts
+++ src/multicall.ts
@@ -1,8 +1,8 @@
 import { Interface } from '@ethersproject/abi';
-import { InitializedPoolUpdate, PoolTokenUpdate } from './types';
 import { CallInput, MultiCall } from '@indexed-finance/multicall';
 import * as bmath from './bmath';
-import { AddressLike, toAddress } from './utils/address';
+import { InitializedPoolUpdate, PoolTokenUpdate, UniswapPairData } from './types';
+import { AddressLike, computeUniswapPairAddress, sortTokens, toAddress } from './utils/address';
 import { BigNumber } from './utils/bignumber';
 import { toProvider } from './utils/provider';
 
@@ -114,4 +114,92 @@ export async function getTokenUserData(
   }
 
   return ret;
+}
+
+type UniswapMultiCallReturn = {
+  tokenABalance?: BigNumber;
+  pairs: UniswapPairData[];
+}
+
+export async function getUniswapData(
+  provider_: any,
+  tokenA_: AddressLike,
+  tokens: AddressLike[],
+  userAddress?: AddressLike
+): Promise<UniswapMultiCallReturn> {
+  const provider = toProvider(provider_);
+  const ierc20Abi = require('./abi/IERC20.json');
+  const pairAbi = require('./abi/Pair.json');
+  const multi = new MultiCall(provider);
+  const calls: CallInput[] = [];
+  const tokenA = toAddress(tokenA_);
+  const tokenAddresses: string[] = [];
+  const pairAddresses: string[] = [];
+  if (userAddress) {
+    calls.push({ target: tokenA, interface: ierc20Abi, function: 'balanceOf', args: [userAddress] });
+  }
+  tokens.forEach((t) => {
+    const tokenB = toAddress(t)
+    tokenAddresses.push(tokenB);
+    const pairAddress = computeUniswapPairAddress(tokenA, tokenB);
+    pairAddresses.push(pairAddress);
+    if (userAddress) {
+      calls.push({ target: tokenB, interface: ierc20Abi, function: 'balanceOf', args: [userAddress] });
+      calls.push({ target: tokenA, interface: ierc20Abi, function: 'allowance', args: [userAddress, pairAddress] });
+      calls.push({ target: tokenB, interface: ierc20Abi, function: 'allowance', args: [userAddress, pairAddress] });
+    }
+    calls.push({ target: pairAddress, interface: pairAbi, function: 'getReserves' });
+  });
+  const response = await multi.multiCall(calls);
+  let tokenABalance: BigNumber | undefined;
+  let i = 0;
+  if (userAddress) {
+    tokenABalance = bmath.bnum(response[i++]);
+  }
+  let chunks = [];
+  let incr = userAddress ? 4 : 1;
+  for (; i < response.length - 1; i += incr) {
+    chunks.push(response.slice(i, i + incr));
+  }
+  const pairs: UniswapPairData[] = [];
+  for (let j = 0; j < chunks.length; j++) {
+    const chunk = chunks[j];
+    const tokenB = tokenAddresses[j];
+    const pairAddress = pairAddresses[j];
+    const [token0,] = sortTokens(tokenA, tokenB);
+    let balanceB: BigNumber | undefined;
+    let allowanceA: BigNumber | undefined;
+    let allowanceB: BigNumber | undefined;
+    let reservesA: BigNumber;
+    let reservesB: BigNumber;
+    let rI = userAddress ? 3 : 1;
+    if (userAddress) {
+      balanceB = bmath.bnum(chunk[0]);
+      allowanceA = bmath.bnum(chunk[1]);
+      allowanceB = bmath.bnum(chunk[2]);
+    }
+    console.log(`J ${j} RI ${rI}`)
+    const [reserves0, reserves1] = chunk[rI];
+    if (token0 == tokenA) {
+      reservesA = bmath.bnum(reserves0);
+      reservesB = bmath.bnum(reserves1);
+    } else {
+      reservesA = bmath.bnum(reserves1);
+      reservesB = bmath.bnum(reserves0);
+    }
+    pairs.push({
+      tokenA,
+      tokenB,
+      pairAddress,
+      balanceB,
+      allowanceA,
+      allowanceB,
+      reservesA,
+      reservesB
+    });
+  }
+  return {
+    tokenABalance,
+    pairs
+  };
 }
