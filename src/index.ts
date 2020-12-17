fe3d985f0292c987949678868c0cabdb8e3f7d56
d1ll0n <dillonkellar@protonmail.com>
2020-12-17 09:31:52 -0600

    removed fn to load pools from cache

--- src/index.ts
+++ src/index.ts
@@ -28,29 +28,3 @@ export async function getAllHelpers(provider_: any, userAddress?: string): Promi
   }
   return { initialized, uninitialized };
 }
-
-export async function getAllHelpersFromCache(provider_: any, userAddress?: string): Promise<{
-  initialized: PoolHelper[],
-  uninitialized: InitializerHelper[],
-}> {
-  const provider = toProvider(provider_);
-  const chainID = (await provider.getNetwork()).chainId;
-  let url = (chainID == 1) ? INDEXED_SUBGRAPH_URL : INDEXED_RINKEBY_SUBGRAPH_URL;
-  let poolDatas;
-  if (chainID == 1) {
-    const { indexPools } = require('./cache/mainnet-pools.json');
-    poolDatas = parsePoolData(indexPools);
-  } else {
-    poolDatas = await getPools(url);
-  }
-  const initialized: PoolHelper[] = [];
-  const uninitialized: InitializerHelper[] = [];
-  for (let poolData of poolDatas) {
-    if (poolData.isPublic) {
-      initialized.push(new PoolHelper(provider, chainID, poolData, userAddress));
-    } else {
-      uninitialized.push(new InitializerHelper(provider, chainID, poolData as UninitializedPool, userAddress));
-    }
-  }
-  return { initialized, uninitialized };
-}
