1ca501ff001ded09e89dd3ac1b6ba3ebdc797f15
d1ll0n <dillonkellar@protonmail.com>
2020-12-23 03:59:39 -0600

    Added helper for staking rewards

---
+++ src/staking/index.ts
@@ -0,0 +1,94 @@
+import { Provider } from "@ethersproject/providers";
+import { toProvider } from "../utils/provider";
+import { bnum } from '../bmath';
+import { StakingPool } from '../types';
+import { BigNumber } from '../utils/bignumber';
+import { getStakingRewardsData } from "./multicall";
+
+export class StakingPoolHelper {
+  protected lastUpdate: number = 0;
+  public updatePromise: Promise<void>;
+
+  public provider: Provider;
+  public userBalanceRewards?: BigNumber;
+  public userEarnedRewards?: BigNumber;
+  public userBalanceStakingToken?: BigNumber;
+  public userAllowanceStakingToken?: BigNumber;
+
+  constructor(
+    provider: any,
+    public chainID: number,
+    public pool: StakingPool,
+    public userAddress?: string
+  ) {
+    this.provider = toProvider(provider);
+  }
+
+  get stakingToken(): string {
+    return this.pool.stakingToken;
+  }
+
+  get rewardsAddress(): string {
+    return this.pool.address;
+  }
+
+  get rewardsFactoryAddress(): string {
+    return this.chainID == 1
+      ? '0x48887E27e3E42e769F34e1e43E857235035d333a'
+      : '0x8d12A344580Bc0bC4E684248067F5d9d3908C864';
+  }
+
+  get shouldUpdate(): boolean {
+    const timestamp = Math.floor(+new Date() / 1000);
+    return timestamp - this.lastUpdate > 300;
+  }
+
+  async waitForUpdate(): Promise<void> {
+    if (this.shouldUpdate) {
+      this.updatePromise = this.updatePool();
+      this.lastUpdate = Math.floor(+new Date() / 1000);
+    }
+    await this.updatePromise;
+  }
+
+  async updatePool(): Promise<void> {
+    const update = await getStakingRewardsData(this.provider, this.pool, this.rewardsFactoryAddress, this.userAddress);
+    const {
+      active,
+      isReady,
+      hasBegun,
+      totalSupply,
+      rewardsDuration,
+      periodStart,
+      periodFinish,
+      lastUpdateTime,
+      claimedRewards,
+      rewardRate,
+      userBalanceRewards,
+      userEarnedRewards,
+      userBalanceStakingToken,
+      userAllowanceStakingToken
+    } = update;
+    this.userBalanceRewards = userBalanceRewards;
+    this.userEarnedRewards = userEarnedRewards;
+    this.userBalanceStakingToken = userBalanceStakingToken;
+    this.userAllowanceStakingToken = userAllowanceStakingToken;
+    Object.assign(this.pool, {
+      active,
+      hasBegun,
+      isReady,
+      totalSupply,
+      rewardsDuration,
+      periodStart,
+      periodFinish,
+      lastUpdateTime,
+      claimedRewards,
+      rewardRate,
+    });
+  }
+
+  async calculateRewardsForDuration(duration: number): Promise<BigNumber> {
+    await this.waitForUpdate();
+    return this.pool.rewardRate.times(duration);
+  }
+}
---
+++ src/staking/multicall.ts
@@ -0,0 +1,86 @@
+import { Provider } from '@ethersproject/providers';
+import { Interface } from '@ethersproject/abi';
+import { CallInput, MultiCall } from '@indexed-finance/multicall';
+import { StakingPool } from '../types';
+import { BigNumber } from '../utils/bignumber';
+import { bnum } from '../bmath';
+
+const rewardsABI = require('../abi/StakingRewards.json');
+const erc20ABI = require('../abi/IERC20.json');
+const factoryABI = require('../abi/StakingRewardsFactory.json');
+
+export interface StakingPoolUpdate {
+  active: boolean;
+  isReady: boolean;
+  hasBegun: boolean;
+
+  totalSupply: BigNumber;
+  rewardsDuration: number;
+  periodStart: number;
+  periodFinish: number;
+  lastUpdateTime: number;
+  claimedRewards: BigNumber;
+  rewardRate: BigNumber;
+
+  userBalanceRewards?: BigNumber;
+  userEarnedRewards?: BigNumber;
+  userBalanceStakingToken?: BigNumber;
+  userAllowanceStakingToken?: BigNumber;
+}
+
+export async function getStakingRewardsData(
+  provider: Provider,
+  pool: StakingPool,
+  factoryAddress: string,
+  userAddress?: string
+): Promise<StakingPoolUpdate> {
+  const iface = new Interface(rewardsABI);
+  const multi = new MultiCall(provider);
+  const calls: CallInput[] = [];
+  calls.push({ interface: iface, target: pool.address, function: 'rewardsDuration' });
+  calls.push({ interface: iface, target: pool.address, function: 'periodFinish' });
+  calls.push({ interface: iface, target: pool.address, function: 'rewardRate' });
+  calls.push({ interface: iface, target: pool.address, function: 'lastUpdateTime' });
+  calls.push({ interface: iface, target: pool.address, function: 'totalSupply' });
+  calls.push({ interface: erc20ABI, target: pool.stakingToken, function: 'balanceOf', args: [pool.address] });
+  calls.push({ interface: factoryABI, target: factoryAddress, function: 'stakingRewardsGenesis' })
+  if (userAddress) {
+    calls.push({ interface: iface, target: pool.address, function: 'balanceOf', args: [userAddress] });
+    calls.push({ interface: iface, target: pool.address, function: 'earned', args: [userAddress] });
+    calls.push({ interface: erc20ABI, target: pool.stakingToken, function: 'balanceOf', args: [userAddress] });
+    calls.push({ interface: erc20ABI, target: pool.stakingToken, function: 'allowance', args: [userAddress, pool.address] });
+  }
+  const response = await multi.multiCall(calls);
+  const rewardsDuration = bnum(response[0]).toNumber();
+  const periodFinish = bnum(response[1]).toNumber();
+  const rewardRate = bnum(response[2]);
+  const lastUpdateTime = bnum(response[3]).toNumber();
+  const totalSupply = bnum(response[4]);
+  const poolRewardsBalance = bnum(response[5]);
+  const stakingRewardsGenesis = bnum(response[6]).toNumber();
+  const timestamp = new Date().getTime() / 1000;
+  const isReady = timestamp >= stakingRewardsGenesis;
+  const active = periodFinish > 0 && lastUpdateTime < periodFinish;
+  const hasBegun = periodFinish > 0;
+  const claimedRewards = hasBegun ? pool.totalRewards.minus(poolRewardsBalance) : bnum(0);
+  const periodStart = hasBegun ? periodFinish - rewardsDuration : 0;
+  const update: StakingPoolUpdate = {
+    active,
+    isReady,
+    hasBegun,
+    rewardsDuration,
+    periodStart,
+    periodFinish,
+    lastUpdateTime,
+    totalSupply,
+    claimedRewards,
+    rewardRate
+  };
+  if (userAddress) {
+    update.userBalanceRewards = bnum(response[7]);
+    update.userEarnedRewards = bnum(response[8]);
+    update.userBalanceStakingToken = bnum(response[9]);
+    update.userAllowanceStakingToken = bnum(response[10]);
+  }
+  return update;
+}
